3b22e65cd371b1e392004ee3cd097d477ead9e7b
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2021-01-13 00:14:42 -0300

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form action="">
+      <input v-model="user" type="text" name="user" id="user">
+      <input v-model="password" type="password" name="password" id="password">
+      <input type="submit" value="Ingresar">
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      user: '',
+      password: '',
+    }
+  }
+}
+</script>
